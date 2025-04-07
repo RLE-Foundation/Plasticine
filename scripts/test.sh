312655f79488ffbba92819bf45eed1105ebf7b2c
yuanmingqi <fredrichyuan19990827@gmail.com>
2025-04-07 09:55:00 -0400

    add procgen

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,7 @@
+# export PYTHONPATH="${PYTHONPATH}:./plasticine"
+
+# CUDA_VISIBLE_DEVICES=1 && python plasticine/standard/ppo_craftax_redo.py
+
 export PYTHONPATH="${PYTHONPATH}:./plasticine"
 
-CUDA_VISIBLE_DEVICES=1 && python plasticine/standard/ppo_craftax_redo.py
+CUDA_VISIBLE_DEVICES=1 && python plasticine/standard/ppo_procgen_vanilla.py
