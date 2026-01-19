49a51beaf62df57ca93074659fc522673cb10f2a
myismyname <mingqi.yuan@connect.polyu.hk>
2026-01-19 12:50:07 +0800

    purge_before_new

--- scripts/test.sh
+++
@@ -1,5 +0,0 @@
-export PYTHONPATH="${PYTHONPATH}:./plasticine"
-
-
-# CUDA_VISIBLE_DEVICES=0 && python plasticine/standard/ppo_procgen_ca.py
-CUDA_VISIBLE_DEVICES=0 && python plasticine/continual/td3_dmc_vanilla.py
